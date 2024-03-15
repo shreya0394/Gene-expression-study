16b811ac3d3b36320100ffabfa1824fc42b53798
shreya0394 <113945002+shreya0394@users.noreply.github.com>
2024-03-14 21:36:14 -0400

    gene_expr_script.R

--- gene_expr_script.R
+++ gene_expr_script.R
@@ -36,7 +36,33 @@ colnames(long_dat)
 result <- long_dat %>%
   left_join(.,modified_met, by = c('samples' = 'description'))
 
-# Filter by required parameter
+# Exploring data, check for any missing values
+any(is.na(result))
+
+# Finding outliers in data
+mean_FPKM <- mean(result$FPKM, na.rm = TRUE)
+sd_FPKM <- sd(result$FPKM, na.rm = TRUE)
+
+# Calculate Z-scores for FPKM values to find outliers with threshold -3 to 3
+Z_score <- (result$FPKM - mean_FPKM) / sd_FPKM
+Z_score
+
+outliers <- result[result$Z_score > 3 | result$Z_score < -3, ]
+outliers
+
+# Statistical analysis
+# t-test for comparing treatment and FPKM values
+group1 <- result[result$treatment == "UNTREATED", "FPKM"]
+group2 <- result[result$treatment == "1.3 mer transfected", "FPKM"]
+
+ttest_result <- t.test(group1, group2)
+ttest_result
+
+# Compare distribution of data based on treatment
+wilcox.test(FPKM ~ treatment, data = result)
+
+
+# Filtering data by required parameter
 filtered_result <- result %>%
   filter(genotype == 'control' | genotype == 'UBR7 knockdown') %>%
   group_by(genes, treatment) %>%
